fa38e2dae78a3c3ef875f4811b2c4e1968ead163
Fabian Fabro <fabrofabian@gmail.com>
2019-02-21 21:14:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function takeANumber(katzDeliLine, name){
+//  var arraycounter= 0;
+    var counter = katzDeliLine.length;
+    katzDeliLine.push(name);
+    //console.log(`Welcome, ${name}. You are number ${counter} in line.`);
+    counter++;
+    //arraycounter++;
+    return (`Welcome, ${name}. You are number ${counter} in line.`);
+  };
+
+  /*
+  for(var counter = 1; counter < katzDeliLine; counter++){
+    katzDeliLine.push(name);
+    console.log(`Welcome, ${name}. You are number ${counter} in line.`);
+  }*/
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!";
+  }
+  while(katzDeliLine.length > 0){
+
+    return "Currently serving "  + katzDeliLine.shift() + ".";
+    //katzDeliLine.shift();
+  }
+
+}
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return "The line is currently empty.";
+  }
+  if(katzDeliLine.length > 0){
+    var peopleInLine;
+    for(var counter = 1; counter < katzDeliLine.length; counter++){
+      peopleInLine = `${counter}. ${katzDeliLine[counter]}, `;
+    }
+  }return "The line is currently: " + peopleInLine;
+}
