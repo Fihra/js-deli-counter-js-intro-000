52571c1f9bec93e4beb562c9403315a8f01c3e28
Fabian Fabro <fabrofabian@gmail.com>
2019-02-21 21:24:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ function currentLine(katzDeliLine){
   }
   if(katzDeliLine.length > 0){
     var peopleInLine;
-    for(var counter = 1; counter < katzDeliLine.length; counter++){
-      peopleInLine = `${counter}. ${katzDeliLine[counter]}, `;
+    for(var counter = 0; counter < katzDeliLine.length; counter++){
+      peopleInLine = `${counter+1}. ${katzDeliLine[counter]}, `;
       
     }return "The line is currently: " + peopleInLine;
   }//return "The line is currently: " + peopleInLine;
